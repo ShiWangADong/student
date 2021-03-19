f0cf4101c50c12e558b02f233a2e5bedc212c486
duhonghao <honghao.du@pinkitec.com>
2021-03-19 13:13:52 +0800

    修改主函数

--- main.py
+++ main.py
@@ -4,14 +4,13 @@ from manage.control.Query import queryAction
 from manage.control.Update import addAction, editAction
 from manage.control.Sort import sortAction
 
-mainAction = ActionItem('', '', [addAction, editAction, deleteAction, queryAction, sortAction],
-                        footerTip=''.center(34, '-'),
-                        headerTip='学生管理系统'.center(30, '-'), prompt='功能菜单', isMain=True)
-ActionItem.mainAction = mainAction
-mainAction.start()
 
-
+# 主函数
 def main():
+    mainAction = ActionItem('', '', [addAction, editAction, deleteAction, queryAction, sortAction],
+                            footerTip=''.center(34, '-'),
+                            headerTip='学生管理系统'.center(30, '-'), prompt='功能菜单', isMain=True)
+    ActionItem.mainAction = mainAction
     mainAction.start()
 
 
